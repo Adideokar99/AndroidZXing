87c6884560a2908c9f1c087fcbad1451c2c6fa5a
3906111 <3906111@fedex.com>
2020-06-16 22:12:39 -0400

    Basic QR Reading

---
+++ app/src/main/java/com/example/android/androidzxing/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.android.androidzxing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.Result;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import org.w3c.dom.Text;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class MainActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+    private ZXingScannerView scannerView;
+    private TextView txtResult;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        scannerView = (ZXingScannerView)findViewById(R.id.zxscan);
+        txtResult = (TextView)findViewById(R.id.txt_result);
+
+        Dexter.withActivity(this)
+                .withPermission(Manifest.permission.CAMERA)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                            scannerView.setResultHandler(MainActivity.this);
+                            scannerView.startCamera();
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+                        Toast.makeText(MainActivity.this, "You must accept this permission", Toast.LENGTH_LONG);
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+
+                    }
+                })
+                .check();
+    }
+
+    @Override
+    protected void onDestroy() {
+        scannerView.stopCamera();
+        super.onDestroy();
+    }
+
+    @Override
+    public void handleResult(Result rawResult) {
+        txtResult.setText(rawResult.getText());
+    }
+}
